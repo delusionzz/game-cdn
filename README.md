36ffeecd04c73d36e51f0ce5edad27f892a6ffd9
illusionTBA <illusionztba@gmail.com>
2024-11-17 11:39:10 -0500

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Delusionz game cdn
+
+## Prerequisite
+
+- [Bun v1.1.35](https://bun.sh)
+
+## Running
+
+To run the dev server run the `bun dev` command and the dev server will be up at localhost:8000
+
+## Adding games
+
+To add a game you first need
+
+- Put the game assets in the right folder in the public/ directory, if the category is not there please add the folder with the category name
+- Please get an image of the game, it can be a screenshot of gameplay or an official image but <strong>must be a webp</strong>.
+- Finally add it to the `list.json` file in the `src/` folder
+
+After doing this you will need to restart the server to see changes.
---
+++ src/index.tsx
@@ -0,0 +1,59 @@
+import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
+import { html, Html } from "@elysiajs/html";
+
+const list = await Bun.file("./src/list.json").json();
+type Game = {
+  name: string;
+  path: string;
+  type: string;
+  img: string;
+};
+
+const app = new Elysia()
+  .use(html())
+  .use(
+    staticPlugin({
+      prefix: "/games",
+      // alwaysStatic: true,
+    })
+  )
+  .get("/list", () => Bun.file("./src/list.json"))
+  .get("/", async () => (
+    <html lang="en">
+      <head>
+        <title>Delusions game CDN</title>
+        <link rel="stylesheet" href="/games/output.css" />
+      </head>
+      <body class="w-full min-h-screen flex flex-col items-center justify-center bg-slate-800">
+        <h1 class="text-3xl text-white">Delusionz CDN Games</h1>
+        <div class="flex flex-wrap w-full justify-center">
+          {list.map((game: Game) => (
+            <div class="m-2 p-2 bg-slate-700 rounded flex w-[20rem] min-h-[10rem] group space-x-2">
+              <img
+                src={game.img}
+                class={
+                  "hover:scale-110 transition-all duration-300 ease-in-out rounded-md object-cover h-[10rem] w-[10rem] "
+                }
+                height={150}
+                width={150}
+                alt={game.name}
+              />
+              <div class="flex flex-col items-center justify-center text-white space-y-2 h-full">
+                <h2 class="text-xl">{game.name}</h2>
+                <h3 class="text-xs p-2 bg-slate-600 rounded-lg">{game.type}</h3>
+                <button onclick={`window.location.href = '${game.path}'`}>
+                  Play
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </body>
+    </html>
+  ))
+  .listen(8000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
